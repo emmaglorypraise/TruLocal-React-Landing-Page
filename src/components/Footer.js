39d2615fe9251837eb289c7bb9352316f5aa079b
emmaglorypraise@gmail.com <emmaglorypraise@gmail.com>
2022-12-16 12:02:16 +0100

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,24 @@
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo2.svg";
 
 export default function Footer() {
   return (
     <>
-      <div className="bg-[#E98100]">
-        <div className="flex items-center pt-[20px] lg:pt-[2px]">
-          <img src={logo} alt="logo" />
+      <div className="bg-[#E98100] ">
+        <div className="max-w-[986px] mx-auto">
+        <div className="flex flex-col text-center justify-center pt-[20px] lg:pt-[2px]">
+          <img src={logo} alt="logo" className="w-1/3  " />
           <p className="font-Bai-Jamjuree font-semibold text-[32px] leading-[44px] tracking-[-0.02em] text-white">We connect people looking to get their to-do list done with people looking to make extra cash.</p>
-          <div className="flex justify-center">
-            <p className="font-Poppins font-medium text-[18px] leading-[27px] text-[#E98100] bg-[#FAEFD7] rounded-[20px] mr-3">Post Job</p>
-            <p className="font-Poppins font-medium text-[18px] leading-[27px] text-[#E98100] bg-[#FAEFD7] rounded-[20px] mr-3">Provide a Service</p>
+          <div className="flex justify-center my-3">
+            <p className="font-Poppins font-medium text-[18px] leading-[27px] text-[#E98100] bg-[#FAEFD7] rounded-[20px] mr-3 p-3">Post Job</p>
+            <p className="font-Poppins font-medium text-[18px] leading-[27px] text-[#E98100] bg-[#FAEFD7] rounded-[20px] mr-3 p-3 ">Provide a Service</p>
           </div>
         </div>
 
         <div className="flex justify-center flex-col md:flex-row">
           <p className="font-Mulish text-white ">Post a Task</p>
+        </div>
+
+        <p></p>
         </div>
       </div>
     </>
